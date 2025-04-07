9503553789ee6bad76452eb0db534432afb505c4
William Yu <60310380+williamyuaus@users.noreply.github.com>
2025-04-07 20:50:53 +1000

    Update Solution.java

--- src/Linked_List/getTheLastKNode/Solution.java
+++ src/Linked_List/getTheLastKNode/Solution.java
@@ -1,5 +1,18 @@
 package Linked_List.getTheLastKNode;
 
 public class Solution {
-    public Node getTheLastKNode(ListNode )
+    public ListNode findKthToTail(ListNode head, int k) {
+        if (head == null || k == 0) {
+            return null;
+        }
+        //int i = 0;
+        ListNode firstNode = head;
+        ListNode secondNode = head;
+        for (int i = 0; i < k - 1; ++i) {
+            if (firstNode.next != null) {
+                firstNode = firstNode.next;
+            }
+            else {
+                secondNode = 
+    }
 }
